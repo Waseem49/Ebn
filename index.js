c9fdb93208947dcf92061c1b53a00b9fcc9e8ebf
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-08-20 14:41:49 +0530

    addtowishlist & skeloton done

---
+++ index.js
@@ -0,0 +1,174 @@
+//---------------------- carousel-start
+const carouselImages = [
+  "https://media.istockphoto.com/id/1051616786/photo/digital-marketing-businessman-working-with-laptop-computer-tablet-and-smart-phone-modern.jpg?s=612x612&w=0&k=20&c=J2A6-q3RtqbISouQVBgpYtI1Ft9KeVsANUFHgG4Olbc=",
+  "https://media.istockphoto.com/id/1071030302/photo/female-inventory-manager-shows-digital-tablet-information-to-a-worker-holding-cardboard-box.jpg?s=612x612&w=0&k=20&c=_PRg90QRTbxUPDljUJEAc9n5WnBG8TkMoEneC554Ag8=",
+  "https://media.istockphoto.com/id/1219980553/photo/online-news-on-a-smartphone-and-laptop-woman-reading-news-or-articles-in-a-mobile-phone.jpg?s=612x612&w=0&k=20&c=QodY8pXN5DbLs3-FhwWhhYKnsOE4Iixky_SwdGitwnQ=",
+  "https://media.istockphoto.com/id/879598534/photo/woman-shopping-for-clothes-online.jpg?s=612x612&w=0&k=20&c=g-RTo0lmKxZczPTbrrwhD3Kk6WUIxGKCilU67tHuhXk=",
+  "https://media.istockphoto.com/id/1177502660/photo/young-woman-reading-the-news-on-a-modern-tablet-computer-while-sitting-in-her-living-room.jpg?s=612x612&w=0&k=20&c=oEfXfMaKkgAVfshd7yk_bxGk2iQncWueLVlTL__gWWg=",
+  "https://media.istockphoto.com/id/887987150/photo/blogging-woman-reading-blog.jpg?s=612x612&w=0&k=20&c=7SScR_Y4n7U3k5kBviYm3VwEmW4vmbngDUa0we429GA=",
+];
+const leftButton = document.getElementById("left");
+leftButton.addEventListener("click", () => moveCarousel(-1));
+
+const rightButton = document.getElementById("right");
+rightButton.addEventListener("click", () => moveCarousel(1));
+
+let currentIndex = 0;
+function moveCarousel(direction) {
+  currentIndex =
+    (currentIndex + direction + carouselImages.length) % carouselImages.length;
+  document.getElementById("cimg").src = carouselImages[currentIndex];
+}
+
+function autoSlide() {
+  moveCarousel(1);
+}
+setInterval(autoSlide, 5000);
+//---------------------- typing-effects ----------
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+};
+
+// --------------------product fetching --------------------
+const api = "https://mock-api-6jin.onrender.com/products";
+const listdiv = document.createElement("listproducts");
+
+// wishlist
+const wishlist = [];
+async function fetchproducts() {
+  try {
+    const response = await fetch(api);
+    const json = await response.json();
+    return json.slice(json.length - 13, json.length - 1).reverse();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return null;
+  }
+}
+fetchproducts();
+
+//----------------- skeloton------------------------
+function displaySkeletons(val) {
+  const listProducts = document.getElementById("listproducts");
+  if (val === undefined) {
+    for (let i = 0; i < 11; i++) {
+      const product = document.createElement("div");
+      product.classList.add("skeleton");
+      const htmlSkeleton = `
+          <div class="skel"></div>
+        `;
+      product.innerHTML = htmlSkeleton;
+      listProducts.appendChild(product);
+    }
+  } else {
+    // Remove all .skeleton elements
+    const skeletonElements = document.querySelectorAll(".skeleton");
+    console.log(skeletonElements);
+    skeletonElements.forEach((skeleton) => {
+      listProducts.removeChild(skeleton);
+    });
+  }
+}
+
+function displayproducts(data) {
+  data?.forEach((el) => {
+    const product = document.createElement("div");
+    product.classList.add("product");
+    const htmlproduct = ` <img
+  src=${el.thumbnail}
+  alt="ecomm" loading="lazy" />
+<h3>${el.title.substring(0, 22)}</h3>
+<p>
+  ${el.description.substring(0, 45) + "..."}
+</p>
+<h4> ${"Price: " + "$" + el.price}</h4>
+<div id="hove">
+  <button class="wishlist" data-product-id="${el.id}">Add to Wishlist</button>
+</div>
+`;
+    product.innerHTML = htmlproduct;
+    // product.insertAdjacentHTML("afterbegin", htmlproduct);
+    document.getElementById("listproducts").appendChild(product);
+  });
+
+  // Add event listeners to the "Add to Cart" buttons
+  const addToCartButtons = document.querySelectorAll(".wishlist");
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.getAttribute("data-product-id");
+      console.log(productId);
+      console.log(data);
+      const productToAdd = data.find((product) => product.id == productId);
+      console.log(productToAdd);
+// ------------------------post to wishlist.----------
+      if (productToAdd) {
+        fetch("https://mock-api-6jin.onrender.com/ebnwishlist", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(productToAdd),
+        });
+      }
+    });
+  });
+}
+async function skelprod() {
+  displaySkeletons();
+  const data = await fetchproducts();
+  if (data) {
+    displayproducts(data);
+    displaySkeletons("delele");
+  }
+}
+skelprod();
