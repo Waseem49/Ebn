44e8bf2e9f388aa1c297e266476cd4c9c95985de
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-11-23 01:30:11 +0530

    infinite scrolling creating issue

--- news/news.js
+++ news/news.js
@@ -3,16 +3,10 @@ const modal = document.getElementById("myModal");
 const modalImage = document.getElementById("modal-image");
 const modalBody = document.getElementById("modal-body");
 const closeBtn = document.getElementsByClassName("close")[0];
-const btnpagi = document.querySelector(".btnpagi");
-btnpagi.style.display = "none";
-const page = document.querySelector("#page");
-const prevbtn = document.querySelector("#prevbtn");
-const nextbtn = document.querySelector("#nextbtn");
 const spinner = document.querySelector(".spinner-box");
 const configure = document.querySelectorAll(".configure-core");
-let scrollhua = 0;
 let pagecount = 1;
-
+let scrollhua = window.scrollY;
 //=============DARKMODE++++++++++++++++
 const darkmode = JSON.parse(localStorage.getItem("darkmode")) || false;
 document.body.style.backgroundColor = darkmode ? "black" : "white";
@@ -23,38 +17,10 @@ configure.forEach((el) => {
 
 //============DARKMODE-----------------
 
-function pageChange() {
-  prevbtn.disabled = pagecount === 1;
-  page.textContent = pagecount;
-}
-pageChange();
-
-nextbtn.addEventListener("click", () => {
-  pagecount++;
-  getnews();
-});
-prevbtn.addEventListener("click", () => {
-  pagecount--;
-  getnews();
-});
-
-window.addEventListener("scroll", scrollval);
-
-function scrollval() {
-  scrollhua = window.scrollY;
-  console.log(window.scrollY);
-}
-
 let newsbody =
   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ut veniam laboriosam facilis ducimus voluptatibus minus ex? Eius illum, veniam quia vitae nulla, sapiente non sed adipisci possimus iure recusandae cumque rem, excepturi nisi debitis est neque molestiae provident! A assumenda consequatur autem cum pariatur. Veritatis corporis voluptas blanditiis dolore eveniet ut, natus omnis commodi ipsam eius assumenda itaque labore dolorem totam laborum reiciendis explicabo magni optio quisquam. Esse alias, ipsum odit quibusdam veritatis minima quia autem ex sed libero tenetur dolor totam, magni officiis perspiciatis rerum accusamus. Inventore quasi error quia est obcaecati unde expedita? Laboriosam deleniti at nihil deserunt iure, maxime fuga, quis distinctio repellendus consequuntur suscipit temporibus minima dolor nobis ducimus eaque, possimus eius ullam porro.";
-btnpagi.style.display = "none";
 spinner.style.display = "none";
-
 async function getnews() {
-  newslist.innerHTML = "";
-  btnpagi.style.display = "none";
-  spinner.style.display = "flex";
-  pageChange();
   const url = `https://newsi-api.p.rapidapi.com/api/category?category=world&language=en&country=us&sort=top&page=${pagecount}&limit=20`;
   const options = {
     method: "GET",
@@ -65,11 +31,16 @@ async function getnews() {
   };
   try {
     const response = await fetch(url, options);
-    const result = await response.json();
-    btnpagi.style.display = "flex";
-    spinner.style.display = "none";
-    result?.map((el) => {
-      const newsdiv = `
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function shownews() {
+  const result = await getnews();
+  result?.map((el) => {
+    const newsdiv = `
         <div id=${el._id}>
           <img src=${
             el.image
@@ -86,17 +57,13 @@ async function getnews() {
           }...</p>
         </div>
       `;
-      newslist.insertAdjacentHTML("afterbegin", newsdiv);
-      const neew = document.getElementById(`${el._id}`);
-      neew.addEventListener("click", () => {
-        modal.innerHTML = "";
-        modal.style.display = "block";
-        newslist.style.display = "none";
-        const newsid = el._id;
-        const singledata = result.filter((el) => {
-          return el._id === newsid;
-        });
-        const modalhtml = `
+    newslist.insertAdjacentHTML("afterbegin", newsdiv);
+    const neew = document.getElementById(`${el._id}`);
+    neew.addEventListener("click", () => {
+      modal.innerHTML = "";
+      modal.style.display = "block";
+      newslist.style.display = "none";
+      const modalhtml = `
        <div class="modal-content">
            <span class="close">&times;</span>
            <div id="news">
@@ -113,23 +80,34 @@ async function getnews() {
            </div>
         </div>
         `;
-        modal.insertAdjacentHTML("afterbegin", modalhtml);
-        var span = document.getElementsByClassName("close")[0];
-        span.onclick = function () {
-          modal.style.display = "none";
-          newslist.style.display = "block";
-        };
-      });
+      modal.insertAdjacentHTML("afterbegin", modalhtml);
+      var span = document.getElementsByClassName("close")[0];
+      span.onclick = function () {
+        modal.style.display = "none";
+        newslist.style.display = "block";
+      };
     });
-    pageChange();
-    // console.log(result);
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
+  });
 }
-getnews();
 
+function showLoader() {
+  spinner.style.display = "flex";
+  setTimeout(() => {
+    spinner.style.display = "none";
+    setTimeout(() => {
+      pagecount++;
+      shownews();
+    }, 300);
+  }, 2000);
+}
+shownews();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 1) {
+    showLoader();
+  }
+});
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
